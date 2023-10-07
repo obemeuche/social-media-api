7f5e93c2e3a950537db71e821882329885e365fd
obemeuche <obemeuchechi@gmail.com>
2023-10-07 17:45:47 +0100

    feat/added database config, log4j, banner.txt and packages

--- src/main/java/com/obemeuche/socialmediaapi/repositories/CommentRepository.java
+++ src/main/java/com/obemeuche/socialmediaapi/repositories/CommentRepository.java
@@ -2,6 +2,8 @@ package com.obemeuche.socialmediaapi.repositories;
 
 import com.obemeuche.socialmediaapi.entities.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
